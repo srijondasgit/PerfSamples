9ae1b40b2ad5c56921367691367a4f488ebb885f
Srijon <srijondas@gmail.com>
2020-05-06 18:54:13 -0700

    Initial commit

---
+++ webservice/src/main/java/com/srijon/webservice/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.srijon.webservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String getHelloWorld(){
+        return "Webservice says hello";
+    }
+
+}
